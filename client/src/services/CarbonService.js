1c155f0dd56db6c571d454b29a38c57b289b61d2
Quentin Crida <qcrida@gmail.com>
2020-01-10 14:51:58 +0000

    footprintservice updated

---
+++ client/src/services/CarbonService.js
@@ -0,0 +1,21 @@
+const baseURL = 'http://localhost:3000/api/footprint/'
+
+export default {
+  getFootprint() {
+    return fetch(baseURL)
+    .then(res => res.json())
+  },
+  postFootprint(payload) {
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+  },
+  deleteFootprint(id) {
+    return fetch(baseURL + id, {
+      method: 'DELETE'
+    })
+  }
+}
