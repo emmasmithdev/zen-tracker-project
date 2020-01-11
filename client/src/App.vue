85055a184d807c436e3f09bab803fd64fa6a6272
Quentin Crida <qcrida@gmail.com>
2020-01-11 12:43:42 +0000

    adding detail to app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,17 @@
 <template lang="html">
 <h1>CO2 and You!</h1>
+<div id="app">
+  <carbon-form />
+  <carbon-score/>
+  <carbon-comparison
+</div>
+
 </template>
 
 <script>
+import CarbonForm from './components/CarbonForm';
+import CarbonScore from './components/CarbonScore';
+import CarbonComparison from './components/CarbonComparison';
 export default {
 }
 </script>
