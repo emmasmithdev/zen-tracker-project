96c196996edf59ab0d63a10337976877ea806ab7
Alan <spambox+github@zenteck.co.uk>
2020-01-13 10:35:17 +0000

    commit to pull

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,12 +10,14 @@
 </template>
 
 <script>
+
   import CarbonForm from './components/CarbonForm';
   import CarbonScore from './components/CarbonScore';
   import CarbonComparison from './components/CarbonComparison';
   import CarbonHistory from './components/CarbonHistory';
   import { eventBus } from './main';
   import CarbonService from './services/CarbonService';
+  import (calculator) from './helpers/CarbonCalculator'
 
   export default {
     name: 'app',
@@ -29,34 +31,37 @@
       return this.footprints[this.footprints.length -1];
     },
     score: function(){
-      let score = 0.0
-      if (this.footprint.drive_car === false){
-        score += 2.4;
-      }
-      if (this.footprint.fly_plane === false){
-        score += 1.6;
-      }
-      if (this.footprint.renewable_energy === true){
-        score += 1.5;
-      }
-      if (this.footprint.vegan === true){
-        score += 0.8;
-      }
-      if (this.footprint.cold_water_wash === true){
-        score += 0.247;
-      }
-      if (this.footprint.recycle === true){
-        score += 0.2125;
-      }
-      if (this.footprint.tumble_dryer === false){
-        score += 0.21;
-      }
-      if (this.footprint.energy_saving_lightbulbs === true){
-        score += 0.1;
-      }
-      return Math.floor(score)
+      return calculator(footprint)
+    }
+//     score: function(){
+//       let score = 0.0
+//       if (this.footprint.drive_car === false){
+//         score += 2.4;
+//       }
+//       if (this.footprint.fly_plane === false){
+//         score += 1.6;
+//       }
+//       if (this.footprint.renewable_energy === true){
+//         score += 1.5;
+//       }
+//       if (this.footprint.vegan === true){
+//         score += 0.8;
+//       }
+//       if (this.footprint.cold_water_wash === true){
+//         score += 0.247;
+//       }
+//       if (this.footprint.recycle === true){
+//         score += 0.2125;
+//       }
+//       if (this.footprint.tumble_dryer === false){
+//         score += 0.21;
+//       }
+//       if (this.footprint.energy_saving_lightbulbs === true){
+//         score += 0.1;
+//       }
+//       return Math.floor(score)
+// }
 }
-},
 
 
 components: {
@@ -64,7 +69,7 @@ components: {
     'carbon-score': CarbonScore,
     'carbon-comparison': CarbonComparison,
     'carbon-history': CarbonHistory
-  },
+  };
 
   mounted() {
        this.fetchData();
--- client/src/components/CarbonScore.vue
+++ client/src/components/CarbonScore.vue
@@ -1,10 +1,35 @@
 <template lang="html">
+  <div id="carbonFootprint">
 
+    <div class="footprint" v-for="footprint in footprints">
+      <p v-if="drive_car === true">You drive a car</p>
+      <p v-elsif="drive_car === false">"You don't own a car"<p>
+      <p>Your score was {{score}} Thunbergs!</p>
+      <button v-on:click="deleteFootprint(footprint._id)">Delete this Footprint</button>
+    </div>
+
+  </div>
 </template>
 
 <script>
+import { eventBus } from '../main.js';
+import CarbonService from '@/services/CarbonService.js'
 export default {
-}
+  name: "carbon-footprint",
+  props: ['footprints', 'score'],
+  filters: {
+    format(value){
+			return new Date(value).toLocaleString().substring(0, 10);
+    }},
+
+  methods: {
+    deleteFootprint(id) {
+      CarbonService.deleteFootprint(id)
+      .then(() => eventBus.$emit('footprint-deleted', id)
+    )}
+
+  }
+  }
 </script>
 
 <style lang="css" scoped>
